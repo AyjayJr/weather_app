e168f58229f4aa8276e7a456e7a8dd28e648c8db
AyjayJr <ayjayjr@hotmail.com>
2023-03-29 17:34:20 -0400

    finished styling of main search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,10 +3,15 @@ const Search = () => {
 
 	return (
 		<div className='card'>
+			<img 
+				className='location-pin'
+				src="./images/location.png" />
 			<input
+				className='search-bar'
 				type='text'
+				placeholder='Enter your location'
 			/>
-			<button>Search</button>
+			<button className='search-button'></button>
 		</div>
 	);
 };
